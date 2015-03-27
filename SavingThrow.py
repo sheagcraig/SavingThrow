8ee5196d10f4540e6839e4014f8ac134d7aee4a3
Shea Craig <shea.craig@da.org>
2015-03-27 12:00:35 -0400

    Add license.

--- SavingThrow.py
+++ SavingThrow.py
@@ -9,6 +9,21 @@ both extension attribute and removal handling are included.
 
 Cleans files as a Casper script policy if --remove is passed as an arg.
 
+Copyright (C) 2015 Shea G Craig <shea.craig@da.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import glob
